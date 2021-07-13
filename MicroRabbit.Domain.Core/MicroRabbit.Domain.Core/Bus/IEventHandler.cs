c2c2d365ea1109e11ca021b9a23bbe747ace3fa2
İsmail ERMİŞ <ismail.ermis@demirorenteknoloji.com>
2021-07-13 18:48:43 +0300

    implemented interfaces and class for Domain.Core

---
+++ MicroRabbit.Domain.Core/MicroRabbit.Domain.Core/Bus/IEventHandler.cs
@@ -0,0 +1,15 @@
+﻿using MicroRabbit.Domain.Core.Events;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Domain.Core.Bus
+{
+    public interface IEventHandler<in TEvent> : IEventHandler
+        where TEvent :Event
+    {
+        Task Handler(TEvent @event);
+    }
+    public interface IEventHandler
+    {
+
+    }
+}
