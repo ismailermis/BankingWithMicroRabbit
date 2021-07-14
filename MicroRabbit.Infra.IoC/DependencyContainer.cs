a60a88c19a62f8dc27a8cd51b84fede556d306c6
İsmail ERMİŞ <ismail.ermis@demirorenteknoloji.com>
2021-07-14 12:59:46 +0300

    Implemented Publish  transfer event Banking microservice

--- MicroRabbit.Infra.IoC/DependencyContainer.cs
+++ MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -3,6 +3,8 @@ using MicroRabbit.Banking.Application.Interfaces;
 using MicroRabbit.Banking.Application.Services;
 using MicroRabbit.Banking.Data.Context;
 using MicroRabbit.Banking.Data.Repository;
+using MicroRabbit.Banking.Domain.CommandHandlers;
+using MicroRabbit.Banking.Domain.Commands;
 using MicroRabbit.Banking.Domain.Interfaces;
 using MicroRabbit.Domain.Core.Bus;
 using MicroRabbit.Infra.Bus;
@@ -18,11 +20,19 @@ namespace MicroRabbit.Infra.IoC
             //Domain Bus
             services.AddTransient<IEventBus,RabbitMQBus>();
             services.AddMediatR(Assembly.GetExecutingAssembly());
+            
+            //Domain Banking commands
+            services.AddTransient<IRequestHandler<CreateTransferCommand,bool>,TransferCommandHandler>();
+
             //Application services
             services.AddTransient<IAccountService, AccountService>();
             //Data
             services.AddTransient<IAccountRepository,AccountRepository>();
             services.AddTransient<BankingDbContext>();
+
+            //
+
+
 
         }
     }
