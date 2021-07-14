2d3a26c6c6ebd20c7185e575cc367e277cbbdcbe
İsmail ERMİŞ <ismail.ermis@demirorenteknoloji.com>
2021-07-14 11:39:40 +0300

    Partialy commited Banking Microservice api

---
+++ MicroRabbit.Banking.Api/Controllers/BankingController.cs
@@ -0,0 +1,33 @@
+﻿using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Banking.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BankingController : ControllerBase
+    {
+        
+
+        private readonly ILogger<BankingController> _logger;
+        private readonly IAccountService _accountService;
+
+        public BankingController(ILogger<BankingController> logger, IAccountService accountService)
+        {
+            _logger = logger;
+            _accountService = accountService;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Account>> Get()
+        {
+             return Ok(_accountService.GetAccounts());
+        }
+    }
+}
--- MicroRabbit.Banking.Api/Startup.cs
+++ MicroRabbit.Banking.Api/Startup.cs
@@ -1,7 +1,10 @@
+using MicroRabbit.Banking.Data.Context;
+using MicroRabbit.Infra.IoC;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -10,6 +13,7 @@ using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace MicroRabbit.Banking.Api
@@ -26,12 +30,22 @@ namespace MicroRabbit.Banking.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            
+            services.AddDbContext<BankingDbContext>(options =>
+            {
+                options.UseSqlite(Configuration.GetConnectionString("BankingDbConnection"));
+            });
             services.AddControllers();
+            RegisterServices(services);
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MicroRabbit.Banking.Api", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Banking Microservice", Version = "v1" });
             });
+        }
+
+        private void RegisterServices(IServiceCollection services)
+        {
+            DependencyContainer.RegiserServices(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
